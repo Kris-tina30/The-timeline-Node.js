8fdf0afa961b5b887b9f12340238f05be1af7e14
Kris-tina30 <kristina.pichkar.15@gmail.com>
2025-04-01 23:24:32 +0200

    change eslint Config function

--- src/server.js
+++ src/server.js
@@ -8,17 +8,22 @@ const PORT = Number(getEnvVar('PORT', '2000'));
 
 const startServer = () => {
   const app = express();
-
+  //static files
   app.use(express.static(path.join(__dirname, '../public')));
 
   //ejs
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, '../src/views'));
+
+  //to submit form
   app.use(express.urlencoded({ extended: true }));
+  //json
   app.use(express.json());
 
+  //cors
   app.use(cors());
 
+  //pino
   app.use(
     pino({
       transport: {
@@ -26,9 +31,9 @@ const startServer = () => {
       },
     }),
   );
-
+  //router
   app.use('/', router);
-
+  //errors
   app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
